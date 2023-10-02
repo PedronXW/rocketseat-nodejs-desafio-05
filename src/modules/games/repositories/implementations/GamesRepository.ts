65b9d7d5cc2bc9392bc05d697cfacb9d446cfaa1
PedronXW <pedroalmeidan@gmail.com>
2023-10-01 21:04:08 -0300

    first commit

---
+++ src/modules/games/repositories/implementations/GamesRepository.ts
@@ -0,0 +1,29 @@
+import { getRepository, Repository } from 'typeorm';
+
+import { User } from '../../../users/entities/User';
+import { Game } from '../../entities/Game';
+
+import { IGamesRepository } from '../IGamesRepository';
+
+export class GamesRepository implements IGamesRepository {
+  private repository: Repository<Game>;
+
+  constructor() {
+    this.repository = getRepository(Game);
+  }
+
+  async findByTitleContaining(param: string): Promise<Game[]> {
+    return await this.repository
+    .createQueryBuilder('games')
+    .where('games.title ILIKE :title', { title: `%${param}%` }).getMany();
+  }
+
+  async countAllGames(): Promise<[{ count: string }]> {
+    return await this.repository.query('SELECT COUNT(*) FROM games');
+  }
+
+  async findUsersByGameId(id: string): Promise<User[]> {
+    const game=await this.repository.createQueryBuilder('games').leftJoinAndSelect('games.users', 'users').where('games.id = :id', { id }).getOne();
+    return game!.users;
+  }
+}
---
+++ src/modules/users/repositories/implementations/UsersRepository.ts
@@ -0,0 +1,30 @@
+import { getRepository, Repository } from 'typeorm';
+
+import { IFindUserByFullNameDTO, IFindUserWithGamesDTO } from '../../dtos';
+import { User } from '../../entities/User';
+import { IUsersRepository } from '../IUsersRepository';
+
+export class UsersRepository implements IUsersRepository {
+  private repository: Repository<User>;
+
+  constructor() {
+    this.repository = getRepository(User);
+  }
+
+  async findUserWithGamesById({
+    user_id,
+  }: IFindUserWithGamesDTO): Promise<User> {
+    return this.repository.findOneOrFail({ where: { id: user_id }, relations: ['games'] })
+  }
+
+  async findAllUsersOrderedByFirstName(): Promise<User[]> {
+    return this.repository.query('SELECT * FROM users ORDER BY first_name ASC');
+  }
+
+  async findUserByFullName({
+    first_name,
+    last_name,
+  }: IFindUserByFullNameDTO): Promise<User[] | undefined> {
+    return this.repository.query('SELECT * FROM users WHERE LOWER(first_name) = LOWER($1) AND LOWER(last_name) = LOWER($2)', [first_name, last_name]);
+  }
+}
